cb0f328277704156d68646f7b707ea91bef5a9f6
gmarsze <98093489+gmarsze@users.noreply.github.com>
2022-02-04 20:10:42 +0200

    Update ll_script.js

--- ll_script.js
+++ ll_script.js
@@ -21,7 +21,7 @@ var geojsonMarkerOptions = {
     weight: 1,
     opacity: 1,
     fillOpacity: 0.8
-};
+}
 
 var geojson1 = new L.GeoJSON.AJAX("./data/ramzor2021.geojson",{
 	style: geojsonMarkerOptions;
