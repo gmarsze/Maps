2f2f47172dc36e05178109aa987d4621c806b8bd
gmarsze <98093489+gmarsze@users.noreply.github.com>
2022-12-16 15:10:05 +0200

    updrd

--- ll_rdsscript.js
+++ ll_rdsscript.js
@@ -2,38 +2,18 @@
 
 var selectedtheme = 0 ;
 
-// -----------------------------------
-
-const redcircle = {
-    radius: 4,
-    fillColor: "#FF0000", // red   //"#ff7800" - orange,
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-const violetcircle = Object.assign({}, redcircle);
-violetcircle.fillOpacity = 0.5;
-violetcircle.fillColor =  "#EE82EE";
-
-const yellowcircle = Object.assign({}, redcircle);
-violetcircle.fillOpacity = 0.1;
-yellowcircle.fillColor = "#FFFF00";
-
-
-const kikar = {
-    radius: 4,
-    fillColor: "#FFFFFF", // white 
-    color: "#9400D3",  // violet
-    weight: 2,
-    opacity: 1,
-    fillOpacity: 0.3
-};
-
-const kikar2 = Object.assign({}, kikar);
-kikar2.radius = 8;
-kikar2.color = "#00FF00";
-kikar2.fillColor = "#0000FF";
+sty1 = function(feature) {
+        return {
+          color: 'red',
+          dashArray: "30 10",
+          weight: 7
+        };
+      }
+		
+black_line = {    
+      color: 'black',
+      weight: 7
+    };
 
 // ------------------------------------------------------------------------
 
@@ -42,7 +22,7 @@ const mapproperties = {
 	initial_lon: 32.06744693937369,
 	initial_lat: 34.82906341552735,
 	initial_zm: 13 
-}
+};
 
 const mapboxlight = {
 	connect: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
@@ -91,51 +71,72 @@ var baseMaps = {
 
 // -----------------------------------------------
 
+
+stylvl1 = function(feature) {
+	lvl = feature.properties.Lvl;
+	if ( lvl == 1 ) {
+		c = 'blue'
+	} else if ( lvl == 2 ) { 
+		c = 'red'
+	} else {
+		c = 'green'
+	}
+
+	return {
+		color: c,
+		//dashArray: "30 10",
+		weight: 5
+	};
+}
+
+
 const lyr1 = { 
-	name: "רמזורים - סוף 2021",
-	url: "https://gmarsze.github.io/Maps/data/ramzor2021.geoJson",
-	style: redcircle,
+	name: "דרכים תמא",
+	url: "https://gmarsze.github.io/Maps/data/TamaRds.geoJson",
+	style: stylvl1,
 	popup: function(feature, layer) {
 		if (feature.properties) {
-				var popupcontent = 'רמזור<br>'+'יישוב: ' + feature.properties.city ;
-				if (feature.properties.TzName!=null)    { popupcontent = popupcontent + '<br> צומת: '+feature.properties.TzName ;} ;
-				if (feature.properties.streets!=null)   { popupcontent = popupcontent + '<br> רחובות: '+feature.properties.streets } ;
-				if (feature.properties.Authority!=null) { popupcontent = popupcontent + '<br> רשות תמרור: '+feature.properties.Authority };
+				popupcontent = feature.properties.NAME + '<br>'+'סיווג: ' + feature.properties.MAVAT_NAME ;
+				if (feature.properties.PLANNO!=null)    { popupcontent = popupcontent + '<br> תכנית: '+feature.properties.PLANNO ;} ;
+				if (feature.properties.REMARKS!=null)   { popupcontent = popupcontent + '<br> הערות: '+feature.properties.REMARKS } ;
 				layer.bindPopup(popupcontent);
-				}  //{ "mahoz","semel","city","nrashut","TzName","streets","Authority","new"}
+				}  
+	},
+	onEachFeature: function (feature, layer) {
+		bindLabel("My Label", {noHide: true, className: "my-label", offset: [0, 0] });
 	}	
 }  
 
-const lyr2 = { 
-	// name: "מעגלי תנועה - סוף 2021",
-	name: "מעגלי תנועה - מפה",
-	url: "https://gmarsze.github.io/Maps/data/kikar2021.geoJson",
-	style: kikar, // yellowcircle,
-	popup: function(feature, layer) {
-		if (feature.properties) {
-				var popupcontent = 'מעגל תנועה<br>'+'יישוב: ' + feature.properties.city ;
-				if (feature.properties.TzName!=null)    { popupcontent = popupcontent + '<br> צומת: '+feature.properties.TzName ;} ;
-				if (feature.properties.streets!=null)   { popupcontent = popupcontent + '<br> רחובות: '+feature.properties.streets } ;
-				if (feature.properties.Authority!=null) { popupcontent = popupcontent + '<br> רשות תמרור: '+feature.properties.Authority };
-				layer.bindPopup(popupcontent);
-				}
-	}	
-}
+/*
+L.geoJson(vessels,{
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup("ID: " + feature.properties.id + "<br>Name: " + feature.properties.name + "<br>DateTime: " + feature.properties.vdatetime + "<br>Speed: " + feature.properties.speedknots + " knots<br>CMG: " + feature.properties.cmg + "°");
+        layer.bindTooltip(feature.properties.name, {permanent: true, direction: 'right'}).openTooltip();
+    }
+}).addTo(map)
+*/
 
-const lyr3 = { 
-	name: "מעגלי תנועה - בנטל",
-	url: "https://gmarsze.github.io/Maps/data/kikarbntl2021.geoJson",
-	style: kikar2, // yellowcircle,
-	popup: function(feature, layer) {
-		if (feature.properties) {
-				var popupcontent = 'מעגל תנועה<br>'+'יישוב: ' + feature.properties.city ;
-				if (feature.properties.TzName!=null)    { popupcontent = popupcontent + '<br> צומת: '+feature.properties.TzName ;} ;
-				if (feature.properties.streets!=null)   { popupcontent = popupcontent + '<br> רחובות: '+feature.properties.streets } ;
-				if (feature.properties.Authority!=null) { popupcontent = popupcontent + '<br> רשות תמרור: '+feature.properties.Authority };
-				layer.bindPopup(popupcontent);
-				}
-	}	
-}
+//	flds = { "ID", "Dir", "Length", "ROADNUMBER", "MAVAT_CODE", "MAVAT_NAME",  "RDNMBR_OLD", "NAME", "LABEL", "REMARKS", "PLANNO",
+//			  "Lvl", "Substract" ,"hov" ,"transit" ,"special" ,"tunnel"}  // 
+
+
+	/*	
+        style: function () {
+            return {
+                color: 'red'
+            }
+        },
+        onEachFeature: function (feature, layer) {
+            layer.on('mouseover', function () {
+                this.setStyle({
+                    color: 'green'
+                });
+            });
+            layer.on('mouseout', function () {
+                geojsonLayer.resetStyle(this);
+            });
+        } */
+
 
 // -----------------------------------------
 
@@ -145,25 +146,38 @@ const lyr3 = {
 var map = L.map('map').setView([mapproperties.initial_lon, mapproperties.initial_lat], mapproperties.initial_zm);
 L.Control.boxzoom({ position:'topleft' }).addTo(map);
 
-mapboxstreetstiles.addTo(map);
+mapboxlighttiles.addTo(map);
 
+var overlayMaps = {};
+var sss = addlyr(map, lyr1, overlayMaps) ;
+//addlyr(map, lyr2, overlayMaps) ;
+//addlyr(map, lyr3, overlayMaps) ;
+
+
+// close ramzor
+//var o1 = overlayMaps[lyr1.name]
+//o1.remove();
+
+
+function addlyr (map, lyr, overlaysObj) {
+	let geojson1 = new L.GeoJSON.AJAX(lyr.url, {	
+		style: lyr.style, 
+//		pointToLayer: function (feature, latlng) {
+//			return L.circleMarker(latlng, lyr.style);
+//			},
+		onEachFeature: lyr.popup
+		});
+	geojson1.addTo(map);
+	overlaysObj[lyr.name] = geojson1;
+}
+
+L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
+
+/*
 var domainTheme = {};
 domainTheme.radius = [ 0, 3, 7 ];
 domainTheme.fillColor = [ 0, "#FFFF00", "#FF0000" ];
 
-var overlayMaps = {};
-var sss = addlyr(map, lyr1, overlayMaps) ;
-addlyr(map, lyr2, overlayMaps) ;
-addlyr(map, lyr3, overlayMaps) ;
-
-var clust1 ;
-var clust2 ;
-
-// close ramzor
-var o1 = overlayMaps[lyr1.name]
-o1.remove();
-
-/*
 function addlyr (map, lyr, overlaysObj) {
 	var geojson1 = new L.GeoJSON.AJAX(lyr.url, {	
 		pointToLayer: function (feature, latlng) {
@@ -180,98 +194,7 @@ function addlyr (map, lyr, overlaysObj) {
 }
 */
 
-function addlyr (map, lyr, overlaysObj) {
-	let geojson1 = new L.GeoJSON.AJAX(lyr.url, {	
-		//style: lyr.style, 
-		pointToLayer: function (feature, latlng) {
-			return L.circleMarker(latlng, lyr.style);
-			},
-		onEachFeature: lyr.popup
-		});
-	geojson1.addTo(map);
-	overlaysObj[lyr.name] = geojson1;
-}
-
-L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
-
 // -------------------------------------------------
-
-function maptheme(src) {
-	selectedtheme = src.value;	
-	//alert(src.value);
-
-	let xlyr1 = overlayMaps[lyr1.name]
-	let xlyr2 = overlayMaps[lyr2.name]
-	let xlyr3 = overlayMaps[lyr3.name]
-
-	if (selectedtheme=="0") {
-		removeclust();
-		addlyrs();
-		mapreset(xlyr1, redcircle);
-		mapreset(xlyr2, kikar);
-		mapreset(xlyr3, kikar2);
-xlyr1.remove();
-	}
-	else if (selectedtheme=="1") {
-		removeclust();
-		addlyrs();
-		mapbyIU(xlyr1);
-xlyr1.remove();
-		mapbyIU(xlyr2, yellowcircle);
-		mapbyIU(xlyr3, violetcircle);
-xlyr3.remove();
-xlyr3.addTo(map);
-	}
-	else if (selectedtheme=="2") { // by new
-		removeclust();
-		addlyrs();
-		mapbynew(xlyr1);
-		mapbynew(xlyr2, yellowcircle);
-	}
-	else if (selectedtheme=="3") { // cluster
-		removelyrs();
-		removeclust();
-		if (clust2!=null) {
-			clust2.remove();
-			}
-		if (clust1!=null) { clust1.addTo(map);;}
-		else {clust1 = mapcluster(xlyr1);}
-	}
-	else if (selectedtheme=="4") { // cluster
-		removelyrs();
-		removeclust();
-		if (clust1!=null) {
-			clust1.remove();
-			}
-		if (clust2!=null) { clust2.addTo(map);;}
-		else {clust2 = mapcluster(xlyr2);}
-	}
-
-}
-
-function removelyrs() {
-	let xlyr1 = overlayMaps[lyr1.name]
-	let xlyr2 = overlayMaps[lyr2.name]
-	xlyr1.remove();
-	xlyr2.remove();
-}	
-
-function removeclust() {
-	if (clust1!=null) {
-		clust1.remove();
-		}
-	if (clust2!=null) {
-		clust2.remove();
-		}
-}	
-
-function addlyrs() {
-	let xlyr1 = overlayMaps[lyr1.name]
-	let xlyr2 = overlayMaps[lyr2.name]
-	xlyr1.addTo(map);
-	xlyr2.addTo(map);
-}	
-
 
 function mapreset(lyr, newstyle) {
 	lyr.eachLayer(function(feature) {
@@ -279,50 +202,8 @@ function mapreset(lyr, newstyle) {
 	});		
 }	
 
-function mapbyIU(lyr, newstyle) {
-	lyr.eachLayer(function(feature) {
-		let elm = feature ;
-		Auth = elm.feature.properties.Authority;
-		if ((Auth!="משרד הבטחון" && Auth!="נתע" && Auth!="רשות שדות התעופה" && Auth!=null)) {
-				if (newstyle!=null) { feature.setStyle(newstyle); }
-				feature.setStyle({
-				radius: 7 
-				});
-		} else {
-			feature.setStyle({
-				//radius: 0.1 ;
-				opacity: 0,
-				fillOpacity: 0
-			});
-		};
-	});		
-}	
 
-function mapbynew(lyr, newstyle) {
-	lyr.eachLayer(function(feature) {
-		let elm = feature ;
-		if (elm.feature.properties.new === 1) {
-				if (newstyle!=null) { feature.setStyle(newstyle); }
-				feature.setStyle({
-				radius: 7 
-				});
-		} else {
-			feature.setStyle({
-				//radius: 0.1 ;
-				opacity: 0,
-				fillOpacity: 0
-			});
-		};
-	});		
-}	
 
-function mapcluster(lyr) {
-	let markers = L.markerClusterGroup();
-	markers.addLayer(lyr); // add it to the cluster group
-	map.addLayer(markers);		// add it to the map
-	map.fitBounds(markers.getBounds()); //set view on the cluster extend
-	return markers;	
-}	
 // -------------------------------------------------------
 
 function test(src) {
